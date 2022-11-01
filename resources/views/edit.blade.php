7c4dd99aeb144a4e1b8ec8d43fc5b0f927ee5053
RocioMarco1011 <rociomarco1011@gmail.com>
2022-11-01 11:20:03 -0600

    LaravelCollective

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,19 +1,19 @@
-@extends("layouts.app")
+@extends('layouts.app')
 @section('title', 'Trainers Edit')
 @section('content')
-<form class="form-group" method="POST" action = "{{action('TrainerContrller@update',$trainer->id)}}" enctype="multipart/form-data">
-    @csrf
-    @method("PUT")
-    <div clas="form-group">
-        <label for="">Nombre:</label>
-        <input type="text" name="name" value="{{$trainer->name}}" class="form-control">
-        <label for="">Apellido:</label>
-        <input type="text" name="apellido" value="{{$trainer->apellido}}" class="form-control">
-    </div>
-    <div clas="form-group">
-        <label for="">Avatar:</label>
-        <input type="file" name="avatar" value="{{$trainer->avatar}}">
-    </div>
-        <button type = "submit" class="btn btn-primary">Editar</button>
-</form>
+{!!Form::model($trainer,['route'=>['trainers.update',$trainer],
+     'method'=>'PUT','files'=>true])!!}
+
+<div class="form-group">
+        {{Form::label('name','Nombre')}}
+        {{Form::text('name',null,['class'=>'form-control'])}}
+        {{Form::label('apellido','Apellido')}}
+        {{Form::text('apellido',null,['class'=>'form-control'])}}
+</div>
+<div class="form-group">
+        {{Form::label('avatar','Avatar')}}
+        {{Form::file('avatar') }}
+</div>
+{{Form::submit('Actualizar',['class'=>'btn btn-primary'])}}
+{!!Form::close()!!}
 @endsection
