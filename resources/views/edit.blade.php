23a49646e5fe35f785b651b6782250a0004520a5
RocioMarco1011 <rociomarco1011@gmail.com>
2022-11-01 11:45:35 -0600

    LaravelCollective

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,19 +1,8 @@
 @extends('layouts.app')
 @section('title', 'Trainers Edit')
 @section('content')
-{!!Form::model($trainer,['route'=>['trainers.update',$trainer],
-     'method'=>'PUT','files'=>true])!!}
-
-<div class="form-group">
-        {{Form::label('name','Nombre')}}
-        {{Form::text('name',null,['class'=>'form-control'])}}
-        {{Form::label('apellido','Apellido')}}
-        {{Form::text('apellido',null,['class'=>'form-control'])}}
-</div>
-<div class="form-group">
-        {{Form::label('avatar','Avatar')}}
-        {{Form::file('avatar') }}
-</div>
+{!!Form::model($trainer,['route'=>['trainers.update',$trainer],'method'=>'PUT','files'=>true])!!}
+@include('form')
 {{Form::submit('Actualizar',['class'=>'btn btn-primary'])}}
 {!!Form::close()!!}
 @endsection
